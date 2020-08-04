b6c39652e77105ecd1b4d8940cdc3c8c5f462d3c
Sean Watson <swatso2020@gmail.com>
2020-08-04 10:45:15 -0400

    update

--- script.js
+++ script.js
@@ -1,44 +1,132 @@
-var eightAm = $("#text-8")
-var nineAm = $("#text-9")
-var tenAm = $("#text-10")
-var elevenAm = $("#text-11")
-var twelevePm = $("#text-12")
-var onePm = $("#text-13")
-var twoPm = $("#text-14")
-var threePm = $("#text-15")
-var fourPm = $("#text-16")
-var fivePm = $("#text-17")
-var sixPm = $("#text-18")
-var sevenPm = $("#text-19")
-var eightPm = $("#text-20")
-var ninePm = $("#text-21")
-var tenPm = $("#text-22")
-var elevenPm = $("#text-23")
-var now = moment().format('MMMM Do YYYY');
-var saveIcon = "./images/save-regular.svg"
 
-const checkTimes = () => {
-    var currentTime = moment().hour();
-    if (currentTime >= 9 && currentTime < 10) {
-      nineAM.addClass("future")
+ var date = moment().format('LL');//create date and format it 
+ $("#date").text(date)//append the date to the welInfro
+var newDay = moment().hour(0);
+
+var scheduledHours = [];
+var availableHours = {};
+var now = moment()
+
+
+console.log(availableHours)
+
+for (var hour = 9; hour < 18; hour++) {
+  scheduledHours.push(moment({hour}).format('h  a'));
+  $('.container').append(`<div class='row time-block' data-time='${hour}'>
+       <!--hour column-->
+           <div class='col-sm col-md-2 hour'>
+             <p>${moment({hour}).format('h  a')}</p>
+           </div>
+        <!--scheduling column-->
+           <div class='col-sm col-md-10 d-flex description'>
+              <div class='input-group'>
+                <textarea class="form-control text-area"></textarea>
+                <div class='input-group-append'>
+                  <button class='save-button d-flex justify-center align-center'>
+                    <i class='far fa-save fa-2x save-icon'></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>`);
+}
+
+//Checking time to determine present, past, or future
+for(var i=9;i<17;i++) {
+    //Get the current hour
+    var amPm="am";
+    
+    if(i>=12) {
+    amPm="pm"; }
+    var hour=i;
+    console.log(hour);
+
+    if(i>12) {
+       
+    hour-=12;
+    console.log(hour)
     }
+    var targetId="#"+hour+amPm;
+   
+    var className="future";
+    
+   
+   
+    // console.log(moment().format("HH:mm A"));
+    // console.log(moment().format("h:mm a"));
+    //If currentTime > i => class future
+   // else if currentTime < i class past
+    // // else class current
+    // $(targetId).addClass(className);
+    }
+    
+
+
+
+//if (currentTime >=0 && currentTime < 9){
+  localStorage.clear();
+//}
+
+//Check for local storage to set value to the object and clearing if currentTime is between 12am and 9am
+if (localStorage.getItem('availableHours')) {
+  availableHours = JSON.parse(localStorage.getItem('availableHours'));
+} else {
+  availableHours = {
+    '9': {
+      time: '9',
+      value: ''
+    },
+    '10': {
+      time: '10',
+      value: ''
+    },
+    '11': {
+      time: '11',
+      value: ''
+    },
+    '12': {
+      time: '12',
+      value: ''
+    },
+    '13': {
+      time: '13',
+      value: ''
+    },
+    '14': {
+      time: '14',
+      value: ''
+    },
+    '15': {
+      time: '15',
+      value: ''
+    },
+    '16': {
+      time: '16',
+      value: ''
+    },
+    '17': {
+      time: '17',
+      value: ''
+    }
+  };
 }
 
-$("#date").text(now)
-var saveNine = $('saveNine');
-const saveNineField = () => {
-  let nineAMText = nineAM.value;
-  localStorage.setItem("nine", nineAMText);
-  console.log(localStorage.getItem("nine"));
-}
-var past = $("#hour-9")
-$(eightAm).addClass("current")
-var nineAm=localStorage.getItem("#9am");
-if(nineAm) {
-$("#9am").val(nineAm);
-}
+//set value of availableHours to equal the user input for each row
+$('.time-block').each(function() {
+  $(this).find('.text-area').val(availableHours[$(this).attr('data-time')].value);
+});
 
+//save value to local storage on click
+$('.save-button').on('click', function(event){
+  event.preventDefault();
 
-console.log(moment().format("MM/DD/YYY"));
-console.log(moment().format("HH:mm A"));
-console.log(moment().format("h:mm a"))
+  //set availableHours time attribute
+  var timeValue = $(this).closest('.time-block').attr('data-time');
+
+  //set availableHours value attribute
+    var textValue = $(this).closest('.time-block').find('.text-area').val();
+    availableHours[timeValue].value = textValue;
+
+  //save user input in each object to local storage
+    localStorage.setItem('availableHours', JSON.stringify(availableHours));
+});
